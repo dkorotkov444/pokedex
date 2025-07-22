f2f1d431cd1bed406e88cc04453dea10988c97f5
DK-CF <korotkovadvisory@gmail.com>
2025-07-22 19:57:03 +0200

    Defined Pokemon array
    Defined empty Pokemon array, then added 6 particular objects to array.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,52 @@
-alert('Hello world!');
-let favoriteFood='Apple';
-document.writeln (favoriteFood);
+/* 
+JavaScript code for Pokedex
+Pokedex is a simple web application for presenting information from Pokemon database.
+*/
+
+// Defines list of Pokemons as array
+let pokemonList = [];
+// Define particular Pokemons as objects with 4 keys. name, height, types, abilities.
+// Define Pikachu
+pokemonList[0] = {
+    name: 'Pikachu',
+    height: 40,
+    types: ['electric'],
+    abilities: ['static', 'lightningrod']
+}
+// Define Raichu
+pokemonList[1] = {
+    name: 'Raichu',
+    height: 80,
+    types: ['electric'],
+    abilities: ['static', 'lightningrod']
+}
+// Define Wartortle
+pokemonList[2] = {
+    name: 'Wartortle',
+    height: 100,
+    types: ['water'],
+    abilities: ['rain-dish', 'torrent']
+}
+// Define Kangaskhan
+pokemonList[3] = {
+    name: 'Kangaskhan',
+    height: 220,
+    types: ['normal'],
+    abilities: ['inner-focus', 'early-bird', 'scrappy']
+}
+// Define Sandshrew
+pokemonList[4] = {
+    name: 'Sandshrew',
+    height: 60,
+    types: ['ground'],
+    abilities: ['sand-veil', 'sand-rush']
+}
+// Define Gloom
+pokemonList[5] = {
+    name: 'Gloom',
+    height: 60,
+    types: ['grass', 'poison'],
+    abilities: ['stench', 'chlorophyll']
+}
+
+
