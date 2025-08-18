2c2dd0f83f668e04de8ffd5b9f87183d638ed139
DK-CF <korotkovadvisory@gmail.com>
2025-08-18 12:05:23 +0200

    Replaced custom Pokemon list and modal with Bootstrap list and modal

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,9 @@ Pokedex is a simple web application for presenting information from Pokémon dat
 */
 
 let pokemonRepository = (function(){
-    // This function defines Pokémon repository and its methods
+    // This IIFE function defines Pokémon repository and its methods
     // Each Pokémon is an objects with 6 keys: name (string), height in meter (number), types (array of strings), abilities (array of strings),
-    //  detailsUrl (string with URL of this Pokémon detail), imageUrl (string with URL of this Pokémon image file).
+    // detailsUrl (string with URL of this Pokémon detail), imageUrl (string with URL of this Pokémon image file).
 
     // Constants and variables definitions
     let pokemonList = [];   // Define list of Pokémons as a blank array
@@ -60,32 +60,7 @@ let pokemonRepository = (function(){
         const foundPokemon= pokemonCalled[0];   // Assign to object first array element, undefined if empty
         return foundPokemon;   // Return an object with specified name if found, undefined otherwise
     }
-/*  OBSOLETE
-    function loadList() {
-        // Loads list of Pokémons from PokéAPI
-        showLoadingMessage();   // Show data loading message
-        let listUrl = `${pokeapiUrl}?limit=${numberPokemons}`;  // PokéAPI URL for all Pokemons
-        return fetch(listUrl)
-            // Parse fetched data
-            .then(response => response.json())
-            .then(json => {
-                // Loop over parsed objects
-                json.results.forEach(item => {  
-                    let capitalizedName = item.name.charAt(0).toUpperCase() + item.name.slice(1);
-                    let pokemon = {
-                        name: capitalizedName,
-                        detailsUrl: item.url
-                    };
-                    // Call function that adds new Pokemon object to repository
-                    add(pokemon);
-                });
-                setTimeout(hideLoadingMessage, 1000);  //Show data loading message for 1 second minimum, then hide
-        }).catch(function (err) {   // Handle errors
-            hideLoadingMessage();   // Hide loading message immediately
-            console.error(err);
-        })
-    }
-*/
+
     function loadPage(offset) {
         // Loads one page of Pokémons from PokéAPI
         showLoadingMessage();   // Show data loading message
@@ -147,114 +122,72 @@ let pokemonRepository = (function(){
         });
     }
 
-    function addListItem(pokemon, container){
+    function addListItem(pokemon){
     // Add one list item with Pokémon button to the list container
-        let listItem = document.createElement('li');    // Create list item
-        let button = document.createElement('button');  //Create button
-        button.classList.add('button__pokemon');
-        button.innerText = pokemon.name;
+        const listItem = document.createElement('li');    // Create list item element
+        const button = document.createElement('button');  //Create button element
+        // Add Bootstrap CSS classes
+        listItem.classList.add('list-group-item')
+        button.classList.add('btn', 'btn-primary', 'w-100');
+
+        button.textContent = pokemon.name;
+
+        // Tie the button to pokemonModal
+        button.setAttribute('data-bs-toggle', 'modal');
+        button.setAttribute('data-bs-target', '#pokemonModal');
+
         // Add event listener to created button, watching out for click
-        button.addEventListener('click',function () {
+        button.addEventListener('click', () => {
             showDetails(pokemon)
         });
         // Append list item with button to the list
         listItem.appendChild(button);
-        container.appendChild(listItem);
+        return listItem;
     }
 
     function showDetails (pokemon) {
     // Displays Pokemon details
-        loadDetails(pokemon).then(function(){
+        loadDetails(pokemon).then(() => {
         // TEST console.log(`${pokemon.name} called up.`);   
-        showModal(pokemon);
-        });
-    }
+        //showModal(pokemon);
+        const modalTitle = document.querySelector('.modal-title');
+        const modalBody = document.querySelector('.modal-body');
+        //const pokemonCard = document.createElement('div');
 
-    function showModal (pokemon) {
-        // This function displays a modal with Pokémon details
-        let modalContainer = document.querySelector('#modal-container');
-        modalContainer.classList.add('is-visible');
+        // Clear modal content
+        modalTitle.textContent = '';
+        modalBody.innerHTML = '';
 
-        // Clear all pre-existing modal container content
-        modalContainer.innerHTML = '';
+        // Compose modal title
+        modalTitle.textContent = `${pokemon.name} details`;
 
-        // Create modal itself
-        let modal = document.createElement('div');
-        modal.classList.add('modal');
-
-        // Add "Close modal" button
-        let closeButton = document.createElement('button');
-        closeButton.classList.add('modal-close');
-        closeButton.innerText = 'Close';
-        closeButton.addEventListener('click', hideModal);
-
-        // Add modal content: title and text
-        let modalTitle = document.createElement('h1');
-        modalTitle.innerText = `${pokemon.name} details`;
-
-        let contentWrapper = document.createElement('div');  // Wrapper for Pokemon image and details
-        contentWrapper.classList.add('modal-content-wrapper');
-
-        // Add modal image: Pokemon s     
-        let modalImage = document.createElement('img');
-        modalImage.src = pokemon.imageUrl;
-        modalImage.alt = `${pokemon.name} default image`;
-        modalImage.classList.add('modal-pokemon-image');
-
-        let modalContent = document.createElement('p');
-        modalContent.innerText = 
+        // Compose modal body in pokemonCard
+        // Add Pokemon image    
+        const pokemonImage = document.createElement('img');
+        pokemonImage.src = pokemon.imageUrl;
+        pokemonImage.alt = `${pokemon.name} default image`;
+        // Add Pokemon details text
+        const pokemonDetails = document.createElement('p');
+        pokemonDetails.innerText = 
             `Height: ${pokemon.height}
              Types: ${pokemon.types.join(', ')}
              Abilities: ${pokemon.abilities.join(', ')}`;
-        modalContent.classList.add('modal-pokemon-text');
 
-        // Appentd image and details to content wrapper
-        contentWrapper.appendChild(modalImage);
-        contentWrapper.appendChild(modalContent);
+        modalBody.appendChild(pokemonImage);
+        modalBody.appendChild(pokemonDetails);
 
-        modal.appendChild(closeButton);
-        modal.appendChild(modalTitle);
-        modal.appendChild(contentWrapper);
-        modalContainer.appendChild(modal);
+        //modalBody.appendChild(pokemonCard);
 
-        // Show modal
-        modalContainer.classList.add('is-visible');
-
-        // Hide modal when user clicks on the modal overlay, but outside the modal itself
-        modalContainer.addEventListener('click',(e) => {
-            if (e.target === modalContainer){
-                hideModal();
-            }
         });
-            
-    }  // End of function showModal
-
-    function hideModal() {
-        // This function hides modal container
-        let modalContainer = document.querySelector('#modal-container');
-        modalContainer.classList.remove('is-visible');
     }
-
-//    document.querySelector('#show-modal').addEventListener('click',() => {
-//        showModal('MODAL TITLE', 'Modal text')
-//    });
-
-    window.addEventListener('keydown',(e) => {          // Hides modal when ESC key is pressed
-        let modalContainer = document.querySelector('#modal-container');
-        if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')){
-            hideModal();
-        }
-    });
-
 
     // Public pokemonRepository methods
     return {
         add: add,                   // undefined
         getAll: getAll,             // returns compele list of Pokémons
         getOne: getOne,             // returns one Pokémon with specified name or undefined
-        addListItem: addListItem,   // undefined
+        addListItem: addListItem,   // listItem element
         showDetails: showDetails,   // undefined
-        //loadList: loadList,       OBSOLETE
         loadPage: loadPage,         // undefined
         loadDetails: loadDetails,   // undefined
         getCurrentOffset: () => currentOffset, // Public method to get offset
@@ -266,7 +199,7 @@ let pokemonRepository = (function(){
 
 //  ************    MAIN PROGRAM    ************
 
-let pokemonRoster = document.querySelector('.pokemon-list');  // Define container element where to attach a list of Pokémons, only once
+const pokemonRoster = document.querySelector('#pokemonList');  // Define container element where to attach a list of Pokémons, only once
 const nextButton = document.querySelector('.next-button');    // Define button calling next Pokémon list page
 const prevButton = document.querySelector('.prev-button');    // Define button calling previous Pokémon list page
 
@@ -278,8 +211,9 @@ function renderPage(offset) {
             return; // Terminate display function
         }
         pokemonRoster.innerHTML = '';   // Clear the list
-        pokemonRepository.getAll().forEach(function(pokemon) {
-            pokemonRepository.addListItem(pokemon, pokemonRoster);
+        pokemonRepository.getAll().forEach(pokemon => {
+            const listItem = pokemonRepository.addListItem(pokemon);    // Store list item returned by addListItem function
+            pokemonRoster.appendChild(listItem);
         });
         // If we are on the first page, hide "Previous page" button
         prevButton.classList.toggle('hidden', offset === 0)
@@ -320,22 +254,7 @@ prevButton.addEventListener('click', () => {
     }
 });
 
-/* OBSOLETE
-// Initialize Pokémon repository from Pokémon API call
-pokemonRepository.loadList().then(function () {
-    if (!pokemonRoster) {
-        console.error('Pokémon list container not found.');  // Log error message
-        return; // Terminate display function
-    }   
-    // Output the list of Pokémons on the homepage using forEach() loop
-    pokemonRepository.getAll().forEach (function(pokemon) {
-        pokemonRepository.addListItem(pokemon, pokemonRoster);
-    }) 
-}).catch(function (err) {
-    // Handle errors
-    console.error(err);
-});
-*/
+
 /*  NOT RELEVANT FOR THE TIME BEING
 // Find one Pokémon by the name entered by user (must come after Pokémon list is created)
 let wanted = prompt('Find Pokémon called: ');
